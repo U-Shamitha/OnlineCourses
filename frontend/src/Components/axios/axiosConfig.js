fb802b257bef5f79778eef7a822c9aaa7139ca3c
U-Shamitha <shamitha19102002@gmail.com>
2024-01-01 22:43:15 +0530

    01-01-24(3)

--- frontend/src/Components/axios/axiosConfig.js
+++ frontend/src/Components/axios/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // Set your base URL here
+  // baseURL: 'http://localhost:5000', // Set your base URL here
+  baseURL: 'https://onlinecourses-server.onrender.com'
 });
 
 export default instance;
